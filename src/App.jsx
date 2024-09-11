9a215a2e21f556208c5f1e51eb764278954709b2
imran <zunaeede@gmail.com>
2024-09-11 09:55:02 +0600

    main - add some image content to the project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
 
 import './App.css'
+import Home from './Home';
 
 function App() {
 
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Ok Tailwind</h1>
-
-      <div>
-        {/* Open the modal using document.getElementById('ID').showModal() method */}
-        <button
-          className="btn"
-          onClick={() => document.getElementById('my_modal_2').showModal()}
-        >
-          open modal
-        </button>
-        <dialog id="my_modal_2" className="modal">
-          <div className="modal-box">
-            <h3 className="font-bold text-lg">Hello!</h3>
-            <p className="py-4">Press ESC key or click outside to close</p>
-          </div>
-          <form method="dialog" className="modal-backdrop">
-            <button>close</button>
-          </form>
-        </dialog>
-      </div>
+      <Home />
     </>
   );
 }
