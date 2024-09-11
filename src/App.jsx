6279525ec381116af94530d111ed2849eac1aeae
imran <zunaeede@gmail.com>
2024-09-11 09:47:35 +0600

    main - setup the component UI.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,25 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Ok Tailwind</h1>
+
+      <div>
+        {/* Open the modal using document.getElementById('ID').showModal() method */}
+        <button
+          className="btn"
+          onClick={() => document.getElementById('my_modal_2').showModal()}
+        >
+          open modal
+        </button>
+        <dialog id="my_modal_2" className="modal">
+          <div className="modal-box">
+            <h3 className="font-bold text-lg">Hello!</h3>
+            <p className="py-4">Press ESC key or click outside to close</p>
+          </div>
+          <form method="dialog" className="modal-backdrop">
+            <button>close</button>
+          </form>
+        </dialog>
+      </div>
     </>
   );
 }
