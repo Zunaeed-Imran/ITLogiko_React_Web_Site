9e03751e4b548ff30d88a0688868728f8deb2ccf
imran <zunaeede@gmail.com>
2024-09-11 10:14:38 +0600

    main - icon package install - fix the theme in the index-html page. - add some content on the footer page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Footer from './Footer';
 import Home from './Home';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Home />
+      <Footer />
     </>
   );
 }
