cf95ada2a9ea9d95b3859e75a0efdfe866e4f43f
imran <zunaeede@gmail.com>
2024-09-11 14:12:41 +0600

    main - add some data in the card item.

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,12 +1,15 @@
 import PropTypes from 'prop-types';
 
-function Card({ logo, title, desc }) {
+function Card({ logo, name, title, desc }) {
   return (
     <div>
       <div className="card card-compact bg-base-100 w-96 shadow-xl">
         <div className="card-body">
-          <h2 className="card-title">{logo}</h2>
-          <h2 className="card-title">{title}</h2>
+          <div className="flex gap-1 justify-center items-center">
+            <h2 className="card-title">{logo}</h2>
+            <h2 className="card-title">{name}</h2>
+          </div>
+          <h2 className="">{title}</h2>
           <p>{desc}</p>
         </div>
       </div>
@@ -21,6 +24,7 @@ export default Card;
 Card.propTypes = {
   // this code will be down to the file
   logo: PropTypes.string,
+  name: PropTypes.string,
   title: PropTypes.string,
   desc: PropTypes.string
 };
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,17 @@
 import Card from "./Card"
+import { GiSpiderWeb } from "react-icons/gi";
 
 function Home() {
   return (
     <div>
-      <Card />
+      <Card
+        logo={<GiSpiderWeb />}
+        name={'Web Application'}
+        title={'Boost Your Business with Web Apps'}
+        desc={'Use web applications to communicate with customerwith employees secure storage of data and to provide data and information to management'}
+      />
     </div>
-  )
+  );
 }
 
 export default Home
